5823323cd9810227aee1a467a23c8f956e2c36fa
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2020-10-17 16:29:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Git-Owner
+
+A simple utility to pretty print the list of files in a git repo and their owners.
+
+ 
---
+++ setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import find_namespace_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='git-owners',
+    use_scm_version={
+        "write_to": "src/_version.py",
+        "write_to_template": 'version = "{version}"\n',
+    },
+    version='0.0.1',
+    scripts=['src/git-owners'],
+    author="Alessandro Crugnola",
+    author_email="alessandro.crugnola@gmail.com",
+    description="A simple utility to generate a report about the owners of files inside a git repo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sephiroth74/git-owners",
+    license="MIT",
+    py_modules=["_version"],
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+    packages=find_namespace_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
